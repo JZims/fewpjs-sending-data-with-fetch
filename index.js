beaf31d2e4d8252bd36cfbf0413aa80cb5782c62
JZims <JZimmsGames@gmail.com>
2021-03-23 00:07:46 -0400

    Finished 1st question

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+
+
+
+
+function submitData(userName, userEmail){
+    const configObject = { 
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            Accept: "application/json"
+            }, 
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail
+    })
+        };
+        
+return fetch("http://localhost:3000/users", configObject)
+.then(response => {return response.json()
+})
+.then(object => console.log(object))
+
+}
